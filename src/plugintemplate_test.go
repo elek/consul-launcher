b0e9dddaf18046d686500d036149a9d8755b7269
Elek, Márton <elek@users.noreply.github.com>
2017-04-25 17:58:52 +0200

    fix unit test

--- src/plugintemplate_test.go
+++ src/plugintemplate_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestProcessContent(testing *testing.T) {
 	content := "{{ env \"YYY\"}}\ntest"
-	result := string(templatePlugin.ProcessContent([]byte(content)))
+	result := string(templatePlugin.ProcessContent([]byte(content), nil))
 	os.ExpandEnv("")
 	if (result != "\ntest") {
 		testing.Error("result is " + result)
